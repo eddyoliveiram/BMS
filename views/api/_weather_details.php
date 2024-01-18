0da4907f274842ed0f522efb1ab38a00a7f7653b
eddy.mesquita <eddy.mesquita@seduc.pa.gov.br>
2024-01-18 16:43:24 -0300

    fixing : improving table responsiveness

--- views/api/_weather_details.php
+++ views/api/_weather_details.php
@@ -2,28 +2,30 @@
 use yii\grid\GridView;
 use app\helpers\WeatherHelper;
 ?>
-<?= GridView::widget([
-	'dataProvider' => new \yii\data\ArrayDataProvider([
-		'allModels' => $currentDayForecast,
-		'pagination' => false, // Disable pagination for only one day
-	]),
-	'columns' => [
-		'date',
-		[
-			'attribute' => 'weekday',
-			'label' => 'Day of the Week',
-			'value' => function ($model) {
-				return WeatherHelper::translateDayOfWeek($model['weekday']);
-			},
+<div class="table-responsive">
+	<?= GridView::widget([
+		'dataProvider' => new \yii\data\ArrayDataProvider([
+			'allModels' => $currentDayForecast,
+			'pagination' => false, // Disable pagination for only one day
+		]),
+		'columns' => [
+			'date',
+			[
+				'attribute' => 'weekday',
+				'label' => 'Day of the Week',
+				'value' => function ($model) {
+					return WeatherHelper::translateDayOfWeek($model['weekday']);
+				},
+			],
+			[
+				'attribute' => 'rain_probability',
+				'label' => 'Rain Probability (%)',
+			],
+			'max',
+			'min',
+			'cloudiness',
+			'rain',
+			'wind_speedy',
 		],
-		[
-			'attribute' => 'rain_probability',
-			'label' => 'Rain Probability (%)',
-		],
-		'max',
-		'min',
-		'cloudiness',
-		'rain',
-		'wind_speedy',
-	],
-]); ?>
+	]); ?>
+</div>
--- views/book/index.php
+++ views/book/index.php
@@ -25,77 +25,78 @@ $this->title = 'Books';
 		'onclick' => 'location.href="/book/create"',
 	]
 ) ?>
+<div class="table-responsive">
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'pager' => [
+            'class' => yii\bootstrap5\LinkPager::class,
+            'options' => [
+                'class' => 'd-flex justify-content-center',
+                'style' => 'margin-top: 20px;',
+            ],
+        ],
 
-<?= GridView::widget([
-	'dataProvider' => $dataProvider,
-	'filterModel' => $searchModel,
-	'pager' => [
-		'class' => yii\bootstrap5\LinkPager::class,
-		'options' => [
-			'class' => 'd-flex justify-content-center',
-			'style' => 'margin-top: 20px;',
-		],
-	],
+        'columns' => [
+            'title',
+            'description',
+            [
+                'attribute' => 'author_id',
+                'label' => 'Author',
+                'value' => function ($model) {
+                    return $model->author->name;
+                },
+                'filter' => \yii\helpers\ArrayHelper::map(\app\models\Author::find()->asArray()->all(), 'id', 'name'),
+            ],
+            [
+                'attribute' => 'pages',
+                'contentOptions' => ['class' => 'text-center'],
+            ],
+            [
+                'attribute' => 'created_at',
+                'format' => ['datetime', 'php:m/d/Y h:i \G\M\T'],
+                'contentOptions' => ['class' => 'text-center'],
+    //			'filter' => Html::input('date', 'BookSearch[created_at]', isset(Yii::$app->request->get('BookSearch')['created_at']) ? Yii::$app->request->get('BookSearch')['created_at'] : '', ['class' => 'form-control', 'placeholder' => 'mm/dd/yyyy']),
+            ],
+            [
+                'label' => 'Edit',
+                'format' => 'raw',
+                'value' => function ($model) {
+                    return Html::tag(
+                        'div',
+                        Html::a('<i class="fas fa-pencil-alt" style="font-size:22px"></i>', ['edit', 'id' => $model->id]),
+                        ['class' => 'text-center']
+                    );
+                },
+            ],
+            [
+                'label' => 'Delete',
+                'format' => 'raw',
+                'value' => function ($model) {
+                    $deleteUrl = ['destroy', 'id' => $model->id];
+                    $confirmMessage = 'Are you sure you want to delete this item?';
+                    $deleteButtonId = 'delete-button-' . $model->id;
 
-	'columns' => [
-		'title',
-		'description',
-		[
-			'attribute' => 'author_id',
-			'label' => 'Author',
-			'value' => function ($model) {
-				return $model->author->name;
-			},
-			'filter' => \yii\helpers\ArrayHelper::map(\app\models\Author::find()->asArray()->all(), 'id', 'name'),
-		],
-		[
-			'attribute' => 'pages',
-			'contentOptions' => ['class' => 'text-center'],
-		],
-		[
-			'attribute' => 'created_at',
-			'format' => ['datetime', 'php:m/d/Y h:i \G\M\T'],
-			'contentOptions' => ['class' => 'text-center'],
-//			'filter' => Html::input('date', 'BookSearch[created_at]', isset(Yii::$app->request->get('BookSearch')['created_at']) ? Yii::$app->request->get('BookSearch')['created_at'] : '', ['class' => 'form-control', 'placeholder' => 'mm/dd/yyyy']),
-		],
-		[
-			'label' => 'Edit',
-			'format' => 'raw',
-			'value' => function ($model) {
-				return Html::tag(
-					'div',
-					Html::a('<i class="fas fa-pencil-alt" style="font-size:22px"></i>', ['edit', 'id' => $model->id]),
-					['class' => 'text-center']
-				);
-			},
-		],
-		[
-			'label' => 'Delete',
-			'format' => 'raw',
-			'value' => function ($model) {
-				$deleteUrl = ['destroy', 'id' => $model->id];
-				$confirmMessage = 'Are you sure you want to delete this item?';
-				$deleteButtonId = 'delete-button-' . $model->id;
+                    $js = <<<JS
+                    $("#{$deleteButtonId}").on("click", function() {
+                        if (confirm("{$confirmMessage}")) {
+                            var deleteUrl = "{$deleteUrl[0]}?id={$model->id}";
+                            window.location.href = deleteUrl;
+                        }
+                    });
+    JS;
+                    $this->registerJs($js);
 
-				$js = <<<JS
-                $("#{$deleteButtonId}").on("click", function() {
-                    if (confirm("{$confirmMessage}")) {
-                        var deleteUrl = "{$deleteUrl[0]}?id={$model->id}";
-                        window.location.href = deleteUrl;
-                    }
-                });
-JS;
-				$this->registerJs($js);
-
-				return Html::tag(
-					'div',
-					Html::a('<i class="fas fa-trash" style="color:red;font-size:22px"></i>', '#', [
-						'id' => $deleteButtonId,
-						'class' => 'delete-link',
-					]),
-					['class' => 'text-center']
-				);
-			},
-		],
-	],
-]); ?>
+                    return Html::tag(
+                        'div',
+                        Html::a('<i class="fas fa-trash" style="color:red;font-size:22px"></i>', '#', [
+                            'id' => $deleteButtonId,
+                            'class' => 'delete-link',
+                        ]),
+                        ['class' => 'text-center']
+                    );
+                },
+            ],
+        ],
+    ]); ?>
+</div>
